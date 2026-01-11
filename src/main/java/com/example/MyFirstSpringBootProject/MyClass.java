9bbe4d28ee564b7d4c9f24ab2e531a24a7f6cdf5
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-11 09:58:38 +0600

    created api2

--- src/main/java/com/example/MyFirstSpringBootProject/MyClass.java
+++ src/main/java/com/example/MyFirstSpringBootProject/MyClass.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyClass {
 
 
-    @GetMapping("")
+    @GetMapping("abc")
     public String sayHello(){
         return "Hello";
 
