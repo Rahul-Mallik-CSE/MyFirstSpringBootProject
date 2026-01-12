9c55543cfaa83263afb0646d384ba211acd1e75f
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-12 10:11:31 +0600

    here the dog is outside from the main package, thats why the bean is not created

---
+++ src/main/java/com/example/something/Dog.java
@@ -0,0 +1,11 @@
+package com.example.something;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "something";
+    }
+}
